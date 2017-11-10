9df3389dbba6598c1388cf06c3a1c00bce639aef
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-10 02:46:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 let currentUser = 'Wali'
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
 const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase () +'!'
+const shortGreeting = 'Welcome,' + slice.currentUser (0)
